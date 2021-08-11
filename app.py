a293943f381a41e2517811c51d200e84c78d8618
Georgeboy <jorge.romero.saray@gmail.com>
2021-08-11 08:22:41 -0500

    pequenas mejoras al home

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-import os
+
 from flask import Flask, render_template, request, url_for, jsonify, abort
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+
 
 app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
-
-credential = ServiceAccountCredentials.from_json_keyfile_name("credentials.json",
-["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets", 
-"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"])
-client = gspread.authorize(credential)
-gsheet = client.open("Clientes sanwise").sheet1
 
 
 @app.route('/')
@@ -22,11 +15,8 @@ def index():
 
 @app.route('/index', methods=["POST"])
 def form():
-    req = request.get_json()
-    row = [req["nombres completos"], req["email"], req["empresa"], req["celular"]]
-    gsheet.insert_row(row, 2)
-    nombres_completos = request.form.get("nombres_completos")
     email = request.form.get("email")
     empresa = request.form.get("empresa")
     celular = request.form.get("celular")
-    return jsonify(gsheet.append(nombres_completos, email, empresa, celular))
+    return jsonify(sh.append(nombres_completos, email, empresa, celular))
+
